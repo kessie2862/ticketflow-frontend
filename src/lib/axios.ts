a3b5f0968192a4a4154f4bd3529cec9f44567b2b
kessie2862 <kayp3477@gmail.com>
2026-01-25 16:44:15 +0000

    feat: add Axios instance with NextAuth token injection

---
+++ src/lib/axios.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { getSession, signOut } from 'next-auth/react';
+
+const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
+
+const axiosInstance = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(async (config) => {
+  const session = await getSession();
+  if (session?.user?.accessToken) {
+    config.headers.Authorization = `Bearer ${session.user.accessToken}`;
+  }
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      await signOut({ callbackUrl: '/login' });
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
